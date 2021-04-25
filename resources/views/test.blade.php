be13476275b61cf74b310baea00ee2abcce00647
Sullen-ui <maorlov.it@gmail.com>
2021-04-25 22:35:21 +0600

    Auth

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,1 +1,11 @@
 <h1>Hello, User!</h1>
+<!-- {{$user->email}} -->
+
+@auth()
+
+<p>Secret text!</p> 
+<h3>{{Auth::user()->name}}</h3>
+<h3>{{Auth::user()->dob}}</h3>
+@endif
+{{\Illuminate\Support\Facades\Auth::check()}}
+
